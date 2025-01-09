21cc52ac86b5643e34b13fd5761085a86cc0c067
RAMYA <kandagatlaramya5@gmail.com>
2025-01-09 22:52:10 +0530

    cors issue for the production

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -10,7 +10,9 @@ const useRestaurantMenu = (resId) => {
   }, []);
 
   const fetchResInfo = async () => {
-    const res = await fetch(RES_INFO + resId);
+    // const res = await fetch(RES_INFO + resId);
+    /**to solve the cors issue, took the response of the kfc and used for every restaurant,  this cors issue is for the production deployment only, for the local is working fine */
+    const res = await fetch('https://run.mocky.io/v3/c8e0ced2-ca52-496c-bf3c-d11340b1433b')
     const json = await res.json();
     setResInfo(json.data);
   };
