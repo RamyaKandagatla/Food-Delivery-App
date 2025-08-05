b28d2dadb07da5f2d2dd4c9707e128584eb66808
RAMYA <kandagatlaramya5@gmail.com>
2025-08-05 09:08:22 +0530

    shimmer component css changes

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,25 +1,20 @@
 const Shimmer = () => {
-    return (
-      <div className="shimmer-cont">
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-      </div>
-    );
-  };
-  
-  export default Shimmer;
-  
+  return (
+    <div className="flex flex-wrap justify-center gap-6 p-6">
+      {Array.from({ length: 16 }).map((_, index) => (
+        <div
+          key={index}
+          className="w-72 h-56 bg-gray-300 animate-pulse rounded-xl shadow-lg"
+        >
+          <div className="w-full h-32 bg-gray-400 rounded-t-xl" />
+          <div className="p-4 space-y-2">
+            <div className="h-4 bg-gray-400 rounded w-3/4"></div>
+            <div className="h-4 bg-gray-400 rounded w-1/2"></div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Shimmer;
